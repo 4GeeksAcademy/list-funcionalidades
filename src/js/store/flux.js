306e3c0f98deb705ca3189abd7b5099053d76460
omar saadane el meski <o_omar_96@hotmail.com>
2024-01-27 18:20:19 +0000

    falta por terminar

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,77 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			//Your data structures, A.K.A Entities
+			url: "https://assets.breatheco.de/apis/fake/contact/",
+			agenda: "LJAgenda",
+			contacts: null,
+			contact: {
+				full_name: "",
+				email: "",
+				agenda_slug: "",
+				address: "",
+				phone: ""
+			},
+			error: null
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			//(Arrow) Functions that update the Store
+			// Remember to use the scope: scope.state.store & scope.setState()
+			handleChange: e => {
 				const store = getStore();
+				let { contact } = store;
+				contact.agenda_slug = store.agenda;
+				contact[e.target.name] = e.target.value;
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				setStore({ contact });
+			},
+			setContact: contact => {
+				setStore({ contact });
+			},
+			getContacts: () => {
+				const store = getStore();
+				fetch(`${store.url}agenda/${store.agenda}`)
+					.then(resp => resp.json())
+					.then(data => setStore({ contacts: data }))
+					.catch(error => setStore({ error }));
+			},
+			addContact: () => {
+				const store = getStore();
+				fetch(`${store.url}`, {
+					method: "POST",
+					body: JSON.stringify(store.contact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => getActions().getContacts())
+					.catch(error => setStore({ error }));
+			},
+			editContact: () => {
+				const store = getStore();
+				fetch(`${store.url}${store.contact.id}`, {
+					method: "PUT",
+					body: JSON.stringify(store.contact),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => getActions().getContacts())
+					.catch(error => setStore({ error }));
+			},
+			delContact: () => {
+				const store = getStore();
+				fetch(`${store.url}${store.contact.id}`, {
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => getActions().getContacts())
+					.catch(error => setStore({ error }));
 			}
 		}
 	};
